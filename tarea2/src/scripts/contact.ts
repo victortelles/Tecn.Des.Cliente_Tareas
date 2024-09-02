ca831f0b7de762bd5cc5039be05634ec03c61664
VMLAP\VMLap <victor.telles@iteso.mx>
2024-09-02 01:45:29 -0600

    Añadiendo estilos al formulario

--- tarea2/src/scripts/contact.ts
+++
@@ -1,7 +0,0 @@
-//Importacion de libreria emailjs
-const emailjs = require('emailjs-com');
-const data = require('data/data.json');
-
-// Inicialización de emailjs
-emailjs.init(data.emailjs.userId);
-
