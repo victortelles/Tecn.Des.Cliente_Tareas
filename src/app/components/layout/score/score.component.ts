e5968ddbf3e8bd89ad2b165fed41ea3a96ada3d2
VMLAP\VMLap <victor.telles@iteso.mx>
2024-09-11 13:51:20 -0600

    Creacion del componente score

---
+++ src/app/components/layout/score/score.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-score',
+  standalone: true,
+  imports: [],
+  templateUrl: './score.component.html',
+  styleUrl: './score.component.scss'
+})
+export class ScoreComponent {
+
+}
