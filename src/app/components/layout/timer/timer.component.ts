64ade98f4af8ec4d43b69f2729a8923ddbf1c656
VMLAP\VMLap <victor.telles@iteso.mx>
2024-09-11 13:51:35 -0600

    Creacion del componente timer o tiempo

---
+++ src/app/components/layout/timer/timer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  standalone: true,
+  imports: [],
+  templateUrl: './timer.component.html',
+  styleUrl: './timer.component.scss'
+})
+export class TimerComponent {
+
+}
