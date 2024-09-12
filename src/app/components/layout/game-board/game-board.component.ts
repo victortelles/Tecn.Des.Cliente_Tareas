f7f18faed2de40f465a51472be32da46a40c3566
VMLAP\VMLap <victor.telles@iteso.mx>
2024-09-11 20:58:40 -0600

    Tablero simple por el momento

--- src/app/components/layout/game-board/game-board.component.ts
+++ src/app/components/layout/game-board/game-board.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game-board',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CommonModule],
   templateUrl: './game-board.component.html',
   styleUrl: './game-board.component.scss'
 })
 export class GameBoardComponent {
-
+  cards = [1,2,3,4,5,6];
 }
