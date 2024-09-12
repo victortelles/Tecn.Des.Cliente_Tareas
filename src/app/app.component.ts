7de2ab9a46c0efae60e7c97636846c930e9d4131
VMLAP\VMLap <victor.telles@iteso.mx>
2024-09-11 20:57:39 -0600

    Estructura global por el momento del HTML.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GameBoardComponent } from './components/layout/game-board/game-board.component';
-import { CardComponent } from './components/layout/card/card.component';
 import { ScoreComponent } from './components/layout/score/score.component';
 import { TimerComponent } from './components/layout/timer/timer.component';
+import { GameBoardComponent } from './components/layout/game-board/game-board.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent, GameBoardComponent, ScoreComponent, TimerComponent],
+  imports: [RouterOutlet, ScoreComponent, GameBoardComponent, TimerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'memorama';
+  moves = 0;
+  matches = 0;
+
+  onMovesChanged(moves: number): void {
+    this.moves = moves;
+  }
+
+  onMatchesChanged(matches: number): void {
+    this.matches = matches;
+  }
 }
