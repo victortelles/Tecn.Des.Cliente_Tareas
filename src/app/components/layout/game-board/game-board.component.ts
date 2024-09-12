90b99ee8b04127e806361c1c7e1c59f764444264
VMLAP\VMLap <victor.telles@iteso.mx>
2024-09-11 22:55:20 -0600

    Pequeños ajustes

--- src/app/components/layout/game-board/game-board.component.ts
+++ src/app/components/layout/game-board/game-board.component.ts
@@ -10,26 +10,6 @@ import { CommonModule } from '@angular/common';
   styleUrl: './game-board.component.scss'
 })
 export class GameBoardComponent {
-  cards = [
-    {content: 'Card 1'},
-    {content: 'Card 2'},
-    {content: 'Card 3'},
-    {content: 'Card 4'},
-    {content: 'Card 5'},
-    {content: 'Card 6'},
-    {content: 'Card 7'},
-    {content: 'Card 8'},
-    {content: 'Card 9'},
-    {content: 'Card 10'},
-    {content: 'Card 11'},
-    {content: 'Card 12'},
-    {content: 'Card 13'},
-    {content: 'Card 14'},
-    {content: 'Card 15'},
-    {content: 'Card 16'},
-    {content: 'Card 17'},
-    {content: 'Card 18'},
-    {content: 'Card 19'},
-    {content: 'Card 20'},
-  ]
+  //Arreglo de 20 cartas
+  cards = Array.from({ length: 20 }, (_, i) => ({ id: i, content: `Card ${i + 1}` }));
 }
