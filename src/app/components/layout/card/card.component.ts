f28773749c75cd97d7418ec711dc65c97f80b08e
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-09-14 14:27:42 -0600

    Cambiando el Estilo de las cartas y implementando el flip de las cartas

--- src/app/components/layout/card/card.component.ts
+++ src/app/components/layout/card/card.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule, NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgClass],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  @Input() card: any;
+  @Input() card: {id : number, content: string, isFlipped: boolean} = {id: 0, content: '', isFlipped: false };
+  @Input() flipCardHandler!: (cardId:number) => void;
+
+  flipCard() {
+    if(!this.card.isFlipped){
+      this.card.isFlipped = !this.card.isFlipped;
+    }
+  }
 }
