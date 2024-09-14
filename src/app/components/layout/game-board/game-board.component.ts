74795a6ea463619d8d91297bc73e6dbbe5963230
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-09-14 14:28:43 -0600

    Implementando mecanica para hacer flip de la carta.

--- src/app/components/layout/game-board/game-board.component.ts
+++ src/app/components/layout/game-board/game-board.component.ts
@@ -9,24 +9,56 @@ import { CommonModule } from '@angular/common';
   templateUrl: './game-board.component.html',
   styleUrls: ['./game-board.component.scss']
 })
+
 export class GameBoardComponent {
-  cards: {id: number, content: string } [] = [];
+  cards: { id: number, content: string, isFlipped: boolean, isMatched: boolean }[] = [];
+  flippedCardIndices: number[] = [];
+  isProcessing: boolean = false;
 
-    constructor () {
-      this.generateCards();
-    }
+  constructor() {
+    this.generateCards();
+  }
 
-    generateCards() {
-      //Funcion para los la creacion de los pares.
-      const contents = ['1','2','3','4','5','6','7','8','9','10'];
+  generateCards() {
+    //Funcion para los la creacion de los pares.
+    const contents = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 
-      //Duplicar el arreglo para hacer los pares
-      const pairedCards = [...contents, ...contents];
+    //Duplicar el arreglo para hacer los pares
+    const pairedCards = [...contents, ...contents];
 
     // Asignar un ID único a cada carta y mezclar el arreglo aleatoriamente
     this.cards = pairedCards.map((content, index) => ({
       id: index,
-      content: content
+      content: content,
+      isFlipped: false,
+      isMatched: false,
     })).sort(() => Math.random() - 0.5);
+  }
+
+  //Funcion para voltear la carta
+  flipCardHandler(index: number) {
+    const card = this.cards[index];
+
+    if (card.isFlipped || this.flippedCardIndices.length === 2) return;
+
+    card.isFlipped = true;
+    this.flippedCardIndices.push(index);
+
+    if (this.flippedCardIndices.length === 2) {
+      setTimeout(() => this.checkForMatch(), 1000);
+    }
+  }
+
+  checkForMatch() {
+    const [firstIndex, secondIndex] = this.flippedCardIndices;
+    const firstCard = this.cards[firstIndex];
+    const secondCard = this.cards[secondIndex];
+
+    if (firstCard.content !== secondCard.content) {
+      firstCard.isFlipped = false;
+      secondCard.isFlipped = false;
+    }
+
+    this.flippedCardIndices = [];
   }
 }
