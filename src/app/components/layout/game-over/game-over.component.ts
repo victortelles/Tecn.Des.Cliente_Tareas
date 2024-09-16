edb4745c2cbbcd483000aed83783afcd0253749a
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-09-15 21:27:24 -0600

    Creacion componente game-over al finalizar juego

---
+++ src/app/components/layout/game-over/game-over.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-over',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-over.component.html',
+  styleUrl: './game-over.component.scss'
+})
+export class GameOverComponent {
+
+}
