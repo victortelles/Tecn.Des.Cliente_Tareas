4e0fa1c8fdd021a61a8d0076a5065f3f86c7c057
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-01 11:18:52 -0600

    Creacion de componentes de usuarios

---
+++ InputOutput/src/app/components/paths/usuarios/detalle-usuarios/detalle-usuarios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-usuarios',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalle-usuarios.component.html',
+  styleUrl: './detalle-usuarios.component.scss'
+})
+export class DetalleUsuariosComponent {
+
+}
