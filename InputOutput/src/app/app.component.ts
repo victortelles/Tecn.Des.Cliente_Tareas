bf478e13fc2950791ec1f92e51b7600a9ddaf6da
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-01 11:51:04 -0600

    Probando los componentes, sin estilo

--- InputOutput/src/app/app.component.ts
+++ InputOutput/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/layouts/navbar/navbar.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { UsuariosComponent } from './components/paths/usuarios/usuarios.component';
+import { ListaUsuariosComponent } from './components/paths/usuarios/lista-usuarios/lista-usuarios.component';
+import { DetalleUsuariosComponent } from './components/paths/usuarios/detalle-usuarios/detalle-usuarios.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, UsuariosComponent, ListaUsuariosComponent, DetalleUsuariosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
