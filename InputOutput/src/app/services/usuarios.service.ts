d5ea3f47b67b4fc2a4185cb7b9168495a17d9a1d
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-01 13:13:37 -0600

    Implementando la solicitud de peticion de la API de usuarios

--- InputOutput/src/app/services/usuarios.service.ts
+++ InputOutput/src/app/services/usuarios.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsuariosService {
+export class UsuarioService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUser(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
 }
