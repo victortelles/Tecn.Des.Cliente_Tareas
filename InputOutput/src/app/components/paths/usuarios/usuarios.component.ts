e016a39bb7697e0251558765cafa2c31ea357086
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-02 21:23:25 -0600

    Implementacion de detalles de usuarios. falta pulir

--- InputOutput/src/app/components/paths/usuarios/usuarios.component.ts
+++ InputOutput/src/app/components/paths/usuarios/usuarios.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { ListaUsuariosComponent } from "./lista-usuarios/lista-usuarios.component";
+import { DetalleUsuariosComponent } from "./detalle-usuarios/detalle-usuarios.component";
+import { User } from '../../../services/usuarios.service';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [],
+  imports: [ListaUsuariosComponent, DetalleUsuariosComponent],
   templateUrl: './usuarios.component.html',
   styleUrls: ['./usuarios.component.scss']
 })
 export class UsuariosComponent {
 
+  selectedUser: User | null = null;
+
+  onUserSelected(user: User) {
+    this.selectedUser = user;
+  }
+
+  onClearUser() {
+    this.selectedUser = null;
+  }
 }
