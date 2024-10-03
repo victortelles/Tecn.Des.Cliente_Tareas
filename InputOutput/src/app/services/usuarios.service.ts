5ecad160c36c136b9701f4d4a5a15766635cb379
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-02 21:43:01 -0600

    Implementacion de mas detalles y agregacion en la interfaz de usuario

--- InputOutput/src/app/services/usuarios.service.ts
+++ InputOutput/src/app/services/usuarios.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@angular/core';
 import axios from 'axios';
 
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
 export interface User {
   id: number;
   name: string;
+  username: string;
   email: string;
+  phone: string;
+  website: string;
+  address: Address;
+  company: Company;
 }
 
 @Injectable({
